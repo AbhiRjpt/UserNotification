4e548445609c86e4ac70784b8844d3bfabb33aa4
Abhimanu Rana <abhimanu@oustlabs.com>
2020-01-13 11:39:28 +0530

    #UserEventNotification: changed firebase key to TestKey

--- commons-module/src/main/java/security/FirebaseSecurity.java
+++ commons-module/src/main/java/security/FirebaseSecurity.java
@@ -2,5 +2,5 @@ package security;
 
 public class FirebaseSecurity {
     public static final String FIREBASE_URL = System.getProperty("FIREBASE_URL","https://fcm.googleapis.com/fcm/send");
-    public static final String AUTH_KEY = System.getProperty("AUTH_KEY","AIzaSyCZYvrMAwyVJ5khztXsvfGHUEkO4wnwHlg");
+    public static final String AUTH_KEY = System.getProperty("AUTH_KEY","TESTKEY");
 }
